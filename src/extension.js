dc69e1ca7725a8a53e6aa3a81fe4f0ccd2fa375c
ZakYeo <zakyeomanson@gmail.com>
2023-06-06 15:42:05 +0100

    Base Application. Includes a "Generate Comments" command in right click menu (or using ctrl+shift+a)

---
+++ src/extension.js
@@ -0,0 +1,85 @@
+const vscode = require('vscode');
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: vscode.workspace.getConfiguration('gptvscode').get('openaiApiKey')
+});
+
+const openai = new OpenAIApi(configuration);
+
+let isRunning = false;
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+    console.log('GPT-VSCode Extension Loaded.');
+
+    let disposable = vscode.commands.registerCommand('gpt-vscode.openai', async function () {
+        // Check if the command is already running
+        if (isRunning) {
+            vscode.window.showErrorMessage("Please wait until the current operation finishes.");
+            return;
+        }
+
+        isRunning = true;
+
+        const editor = vscode.window.activeTextEditor;
+
+        if (editor) {
+            let document = editor.document;
+            let selection = editor.selection;
+
+            // Get the text the user has highlighted
+            let highlightedText = document.getText(selection);
+
+            // If no text was selected, show an error
+            if (!highlightedText) {
+                vscode.window.showErrorMessage("Please select some text first.");
+                isRunning = false;
+                return;
+            }
+
+            // Get the language of the current file
+            let language = document.languageId;
+
+            // Display a progress indicator
+            vscode.window.withProgress({
+                location: vscode.ProgressLocation.Notification,
+                title: "Generating comments. Please Wait...",
+                cancellable: false
+            }, async (progress) => {
+                try {
+                    let response = await openai.createChatCompletion({
+                        model: 'gpt-3.5-turbo',
+                        messages: [{ role: 'user', content: `Add suitable comments and docstrings to the following code. Follow conventions and standards. The language is ${language}. Only include the code in your response: ` + highlightedText }],
+                    });
+
+                    // Replace the highlighted text with the response from OpenAI API
+                    editor.edit((editBuilder) => {
+                        editBuilder.replace(selection, response.data.choices[0].message.content);
+                    });
+
+                    // Display a new notification with a completion message
+                    vscode.window.showInformationMessage("Comments generated.");
+
+                } catch (error) {
+                    vscode.window.showErrorMessage('An error occurred while contacting the OpenAI API: ' + error.message);
+                    console.error(error);
+                } finally {
+                    isRunning = false;
+                }
+            });
+        }
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+}
