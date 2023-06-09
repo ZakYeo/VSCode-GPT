dd802b8e4b0623ac4a5c3c5af9432c7c1a5c6f08
ZakYeo <zakyeomanson@gmail.com>
2023-06-09 12:49:12 +0100

    Added more comments

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -3,27 +3,45 @@ const { interactWithOpenAI } = require("./openaiInteractions");
 
 const path = require('path');
 
+/**
+ * Class representing a tree item in Visual Studio Code extensions.
+ * @extends vscode.TreeItem
+ */
 class MyTreeItem extends vscode.TreeItem {
+    /**
+     * Create a tree item.
+     * @param {string} label - The label of the tree item.
+     * @param {string} iconPath - The path to the icon of the tree item.
+     * @param {vscode.TreeItemCollapsibleState} collapsibleState - The collapsible state of the tree item.
+     */
     constructor(label, iconPath, collapsibleState) {
         super(label, collapsibleState);
-        this.iconPath = iconPath;
-        this.children = [];
+        this.iconPath = iconPath;  // Path to the icon to be displayed with this item
+        this.children = [];  // Array to store child nodes
     }
 }
 
+/**
+ * Class acting as a Data Provider in Visual Studio Code extensions.
+ * It populates a tree view with data and handles changes in the data.
+ */
 class MyDataProvider {
+    /**
+     * Create a data provider.
+     */
     constructor() {
         this._onDidChangeTreeData = new vscode.EventEmitter();
         this.onDidChangeTreeData = this._onDidChangeTreeData.event;
-        this.data = [];
-        this.currentConversation = null;
+        this.data = [];  // Array to store tree data
+        this.currentConversation = null;  // Variable to store the currently selected conversation
     }
 
+    // Refresh the tree view UI
     refresh() {
         this._onDidChangeTreeData.fire();
     }
     
-
+    // Add a message to the tree view
     addMessage(sender, message) {
         let iconPath;
         if (sender === 'AI') {
@@ -42,6 +60,7 @@ class MyDataProvider {
         this.refresh();
     }
 
+    // Return a tree item
     getTreeItem(element) {
         element.command = {
             command: 'gpt-vscode.openai.selectConversation',
@@ -51,6 +70,7 @@ class MyDataProvider {
         return element;
     }
 
+    // Return children of a tree item
     getChildren(element) {
         if (element) {
             return element.children;
@@ -59,6 +79,7 @@ class MyDataProvider {
         }
     }
 
+    // Return path to the icon for a sender
     getIconPath(sender) {
         return {
             light: path.join(__filename, '..', '..', 'resources', `${sender.toLowerCase()}.svg`),
@@ -66,12 +87,14 @@ class MyDataProvider {
         };
     }
 
+    // Add a parent item to the tree view
     addParent(label, collapsibleState) {
         const parent = new MyTreeItem(label, path.join(__filename, '..', '..', 'resources', 'icon.svg'), collapsibleState);
         this.data.push(parent);
         this.refresh();
     }
 
+    // Add a child item to a parent item in the tree view
     addChild(parentLabel, label, collapsibleState, sender) {
         const parent = this.data.find((node) => node.label === parentLabel);
         if (parent) {
@@ -82,6 +105,7 @@ class MyDataProvider {
         }
     }
 
+    // Start a chat with the AI
     async chatWithAI(context) {
         let selectedChat = this.currentConversation;
 
@@ -123,10 +147,12 @@ class MyDataProvider {
     }
     }
 
+    // Select a conversation
     selectConversation(label) {
         this.currentConversation = label;
     }
 
+    // Create a new conversation
     createNewConversation(context) {
         let conversationCounter = context.globalState.get('conversationCounter', 1); // Use a default value of 1
         let newChatLabel = "Conversation " + conversationCounter++;
@@ -138,7 +164,7 @@ class MyDataProvider {
 }
 
 
-
+// Export the Data Provider class
 module.exports = {
     MyDataProvider
 }
