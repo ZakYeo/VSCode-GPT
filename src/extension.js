7a7bb74b0ae8133d2c92cb082099cbb94349a489
ZakYeo <zakyeomanson@gmail.com>
2023-06-09 12:40:02 +0100

    Added comments

--- src/extension.js
+++ src/extension.js
@@ -2,26 +2,29 @@ const vscode = require('vscode');
 const { MyDataProvider } = require("./dataProvider");
 const { generateComments, generateCode } = require("./openaiInteractions");
 
-
 /**
  * @param {vscode.ExtensionContext} context
+ * The method that gets called when your extension is activated
+ * @description Activation function for the extension, responsible for registering commands and initializing the data provider.
  */
 function activate(context) {
     console.log('GPT-VSCode Extension Loaded.');
 
+    // Register command to generate comments using GPT
     let disposable = vscode.commands.registerCommand('gpt-vscode.openai.generateComments', async function () {
         await generateComments();
     });
 
+    // Register command to generate code using GPT
     let disposableGenerateCode = vscode.commands.registerCommand('gpt-vscode.openai.generateCode', async function () {
         await generateCode();
     });
 
+    // Initialize data provider and register it
     let myDataProvider = new MyDataProvider();
     vscode.window.registerTreeDataProvider('myListView', myDataProvider);
 
-    
-    // load saved conversations from storage
+    // Load saved conversations from storage and populate them in the data provider
     let savedConversations = context.globalState.get('conversations', []); // Use a default value of an empty array
     for (let conversation of savedConversations) {
         myDataProvider.addParent(conversation.label, vscode.TreeItemCollapsibleState.Expanded);
@@ -30,19 +33,22 @@ function activate(context) {
         }
     }
 
-
+    // Register command to create a new conversation
     let disposableNewConversation = vscode.commands.registerCommand('gpt-vscode.openai.addEntry', async function () {
         myDataProvider.createNewConversation(context);
     });
 
+    // Register command to select an existing conversation
     let disposableSelectConversation = vscode.commands.registerCommand('gpt-vscode.openai.selectConversation', function (label) {
         myDataProvider.selectConversation(label);
     });
 
+    // Register command to chat with AI
     let disposableChat = vscode.commands.registerCommand('gpt-vscode.openai.chatWithAI', async function () {
         myDataProvider.chatWithAI(context);
     });
 
+    // Add the disposable commands to the context
     context.subscriptions.push(disposableNewConversation);
     context.subscriptions.push(disposableSelectConversation);
     context.subscriptions.push(disposableChat);
@@ -50,10 +56,13 @@ function activate(context) {
     context.subscriptions.push(disposableGenerateCode);
 }
 
-
-// This method is called when your extension is deactivated
+/**
+ * The method that gets called when your extension is deactivated
+ * @description Deactivation function for the extension. Add any necessary cleanup tasks here.
+ */
 function deactivate() {}
 
+// Export the activate and deactivate functions so that they can be used by VS Code
 module.exports = {
     activate,
     deactivate
